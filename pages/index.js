131163f8468341dbd48fa2f9482cc5632d7eab3d
dipanc1 <dipanchhabra@gmail.com>
2022-02-10 15:06:26 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Blogs</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+
       <main className={styles.main}>
+
+
         <h1 className={styles.title}>
           morph!
         </h1>
@@ -21,34 +32,23 @@ export default function Home() {
           a noob trying to make his first blog website!
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="blogs">
+          <div className="blogItem">
+            <h1>changing my career path</h1>
+            <p>how i switched from mechanical engineering to software development</p>
+          </div>
+          <div className="blogItem">
+            <h1>changing my career path</h1>
+            <p>how i switched from mechanical engineering to software development</p>
+          </div>
+          <div className="blogItem">
+            <h1>changing my career path</h1>
+            <p>how i switched from mechanical engineering to software development</p>
+          </div>
+          <div className="blogItem">
+            <h1>changing my career path</h1>
+            <p>how i switched from mechanical engineering to software development</p>
+          </div>
         </div>
       </main>
 
