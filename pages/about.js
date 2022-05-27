9f892cbcc3756c32dd958947d59976e3afb70804
dipanc1 <dipanchhabra@gmail.com>
2022-05-27 16:22:13 +0530

    about page

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from '../styles/About.module.css'
 
 const About = () => {
   return (
-    <div> es ist ein About page</div>
+    <div className={styles.container}> 
+      <h1 className={styles.center}>About morph</h1>
+      <h2>Introduction</h2>
+      <p>This is the morph. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi tempore neque consectetur quas aut accusantium quibusdam nesciunt, ab quidem, culpa dolorem quae voluptate?</p>
+      <h2>What is it?</h2>
+      <p>This is the morph. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi tempore neque consectetur quas aut accusantium quibusdam nesciunt, ab quidem, culpa dolorem quae voluptate?</p>
+      <h2>How does it work?</h2>
+      <p>This is the morph. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi tempore neque consectetur quas aut accusantium quibusdam nesciunt, ab quidem, culpa dolorem quae voluptate?</p>
+    </div>
   )
 }
 
